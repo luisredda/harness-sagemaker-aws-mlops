5b51da22dbe1a16459621f372e61cb5c409562ae
Luis Redda <luis.redda@gmail.com>
2023-03-16 10:57:36 -0300

    Update train-deploy.py

--- train-deploy.py
+++ train-deploy.py
@@ -18,9 +18,9 @@ model = Estimator(
     image_uri=image,
     role=role,
     base_job_name='tf-custom-container-test-job',
-    training_instance_count=int(sys.argv[2]),
-    training_instance_type=sys.argv[3],
-    sagemaker_session=session,
+    instance_count=int(sys.argv[2]),
+    instance_type=sys.argv[3],
+    sagemaker_session=session
 )
 
 pprint(vars(model))
